241903c613a249ccec6ae934c86813550aaa23cf
M.Z.MOHAMMED MOHIDEEN <technicalaasaan@gmail.com>
2023-04-04 18:14:10 +0530

    server load static

--- LMS/settings.py
+++ LMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%glbc7snwnuoeapxpgu)xkdsbprq1+o)&17)svz2iyzei32^^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', ]
 
